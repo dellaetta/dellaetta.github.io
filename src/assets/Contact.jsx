17dc17c1690d28cf8d1459b8e8b2e8336a853350
chloev <138642390+dellaetta@users.noreply.github.com>
2026-06-08 17:24:02 -0400

    fixed linkedin link

--- src/assets/Contact.jsx
+++ src/assets/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.css';
 function Contact({type}) {
     return ( 
         <ul className={'social-links--' + type}>
-          <li><a href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile' target='_blank' className = 'fa fa-linkedin'/></li>
+          <li><a href='https://www.linkedin.com/in/chloe-velez-10559b35a' target='_blank' className = 'fa fa-linkedin'/></li>
           <li><a href='https://github.com/dellaetta' target='_blank' className = 'fa fa-github'/></li>
           <li><a href='https://www.instagram.com/chloeblue107/' target='_blank' className = 'fa fa-instagram'/></li>
         </ul>
